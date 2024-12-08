e261034c5c2cd7577503ec25cc4d71ec1a117ace
THE-LEGEND-OF-NITYA <aryannitya141@gmail.com>
2024-12-08 09:46:54 +0530

    Update 0_AADI-CHICKEN.js

--- Priyansh/commands/0_AADI-CHICKEN.js
+++ Priyansh/commands/0_AADI-CHICKEN.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "chicken",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "𝐌.𝐑 𝐀𝐑𝐘𝐀𝐍", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "chicken",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("chicken")==0 || event.body.indexOf("Chicken")==0 || event.body.indexOf("CHICKEN")==0 || event.body.indexOf("CHIKAN")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐀𝐚𝐝𝐢 𝐛𝐚𝐛𝐮 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n_______________________",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐌.𝐑 𝐀𝐑𝐘𝐀𝐍 \n_______________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐡𝐢𝐜𝐇𝐞𝐍\n_______________________",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/62306739ed186731b1f646029335e853.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
